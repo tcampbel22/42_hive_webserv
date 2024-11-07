c42d40033530a20c6ad4dfb7ed228a8693b55659
Timothy Campbell <tcampbel@student.hive.fi>
2024-11-07 11:31:21 +0200

    Added print statements to function failures

--- src/HttpServer.cpp
+++ src/HttpServer.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   HttpServer.cpp                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: clundber < clundber@student.hive.fi>       +#+  +:+       +#+        */
+/*   By: tcampbel <tcampbel@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 10:18:33 by clundber          #+#    #+#             */
-/*   Updated: 2024/11/07 10:58:19 by clundber         ###   ########.fr       */
+/*   Updated: 2024/11/07 11:30:31 by tcampbel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,15 +30,17 @@ void HttpServer::startServer()
 		return ; //change later and remember to close all fds
 	if (listen(_serverFd, 5) < 0)
 	{
+		std::cout << "listen failed\n";
 		return ; //change later, also close all fds
 	}
-	
-	// epollFd = epoll_create1(0);
-	// _events.events = EPOLLIN;
-	// _events.data.fd = _serverFd;
-	// if (epoll_ctl(epollFd, EPOLL_CTL_ADD, _socket, &_events) < 0)
-	// 	return ; //change later
 }
+
+void	setNonBlocking(int socket)
+{
+	int	flag = fcntl(socket, F_GETFL, 0); //retrieves flags/settings from socket
+	fcntl(socket, F_SETFL, flag | O_NONBLOCK); //Sets socket to be nonblocking
+}
+
 
 void HttpServer::startListening()
 {
@@ -48,40 +50,33 @@ void HttpServer::startListening()
 
 	std::string response2 = "HTTP/1.1 200 OK\nContent-Type: text/html; charset=UTF-8\nContent-Length: 137\n\n<!DOCTYPE html>\n<html>\n<head>\n    <title>Simple C++ Web Server</title>\n</head>\n<body>\n    <h1>Hello from a shitty C++ web server!</h1>\n</body>\n</html>\n";
 
-	
 	 epollFd = epoll_create1(0);
-	// int	flag = fcntl(_serverFd, F_GETFL, 0); //retrieves flags/settings from socket
-	// fcntl(_serverFd, F_SETFL, flag | O_NONBLOCK); //Sets socket to be nonblocking
 	_events.data.fd = _serverFd;
 	 _events.events = EPOLLIN;
 	epoll_ctl(epollFd, EPOLL_CTL_ADD, _serverFd, &_events); //maybe protect
-
+	
 	while (true)
 	{
 		numEvents = epoll_wait(epollFd, _eventsArr, MAX_EVENTS, 0);
 		if (numEvents < 0){
-			//print error
+			std::cout << "Epoll wait failed\n";
 			break ;}
 			
-			// std::cout << numEvents << "\n";
 		for (int i = 0; i < numEvents; i++)
 		{
-			std::cout << "got here" << std::endl;
 			if (_eventsArr[i].data.fd == _serverFd)
 			{
 				socklen_t _sockLen = sizeof(_socketInfo);
 				_clientSocket = accept(_serverFd, (sockaddr *)&_socketInfo, &_sockLen);
 				if (_clientSocket < 0) 
 				{
-					
-					//print error
+					std::cout << "accept failed\n";
 					continue;
 				}
 				std::cout << "New client connected: " << _clientSocket << std::endl;
-
-				int	flag = fcntl(_clientSocket, F_GETFL, 0); //retrieves flags/settings from socket
-				fcntl(_clientSocket, F_SETFL, flag | O_NONBLOCK); //Sets socket to be nonblocking
-                _events.events = EPOLLIN | EPOLLOUT | EPOLLET;
+				setNonBlocking(_clientSocket);
+            	
+				_events.events = EPOLLIN | EPOLLOUT | EPOLLET;
                 _events.data.fd = _clientSocket;
 				
 				epoll_ctl(epollFd, EPOLL_CTL_ADD, _clientSocket, &_events); //guard later
@@ -90,10 +85,11 @@ void HttpServer::startListening()
 			{	
 				int _fd_out = _eventsArr[i].data.fd;
 				std::cout << "out fd =" << _fd_out << std::endl;
-				if (_fd_out % 3 == 0)
+				if (_fd_out % 2 == 0)
 					send(_fd_out, response.c_str(), response.size(), 0);
 				else
 					send(_fd_out, response2.c_str(), response2.size(), 0);
+				continue;
 			}
 			
 		}
@@ -102,8 +98,6 @@ void HttpServer::startListening()
 	std::cout << "outofloop" << std::endl;
 	//close (_clientSocket);
 	close (epollFd);
-	
-
 }
 
 void HttpServer::closeServer()
