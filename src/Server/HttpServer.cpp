b3bbfea145787f26df9cb0208b979371c8565a2f
Timothy Campbell <tcampbel@student.hive.fi>
2024-12-19 17:18:49 +0200

    Added error handling for epoll create

--- src/Server/HttpServer.cpp
+++ src/Server/HttpServer.cpp
@@ -161,7 +161,6 @@ void HttpServer::startListening()
 		}
 		fdActivityLoop(current_time);
 	}
-	close(epollFd);
 }
 
 void	HttpServer::addServerToEpoll()
@@ -181,6 +180,7 @@ void	HttpServer::addServerToEpoll()
 		server_node->serverPtr = &settings_vec[i];
 		_events.data.ptr = server_node;
 		server_nodes.push_back(server_node);
+		setNonBlocking(server_node->fd);
 		
 		if (epoll_ctl(epollFd, EPOLL_CTL_ADD, settings_vec[i]._fd, &_events) == -1)		
 		{
